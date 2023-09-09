c90a675dca34672dcf825627cbb12356ee18c412
AAweidai <dw1253464613@gmail.com>
2023-09-09 13:33:09 +0800

    Fix unsafe

--- src/index.rs
+++ src/index.rs
@@ -276,15 +276,15 @@ impl<T> BitcoinCoreRpcResultExt<T> for Result<T, bitcoincore_rpc::Error> {
     match self {
       Ok(ok) => Ok(Some(ok)),
       Err(bitcoincore_rpc::Error::JsonRpc(bitcoincore_rpc::jsonrpc::error::Error::Rpc(
-        bitcoincore_rpc::jsonrpc::error::RpcError { code: -8, .. },
-      ))) => Ok(None),
+                                            bitcoincore_rpc::jsonrpc::error::RpcError { code: -8, .. },
+                                          ))) => Ok(None),
       Err(bitcoincore_rpc::Error::JsonRpc(bitcoincore_rpc::jsonrpc::error::Error::Rpc(
-        bitcoincore_rpc::jsonrpc::error::RpcError { message, .. },
-      )))
-        if message.ends_with("not found") =>
-      {
-        Ok(None)
-      }
+                                            bitcoincore_rpc::jsonrpc::error::RpcError { message, .. },
+                                          )))
+      if message.ends_with("not found") =>
+        {
+          Ok(None)
+        }
       Err(err) => Err(err.into()),
     }
   }
@@ -361,7 +361,7 @@ impl Index {
         let tx = database.begin_write()?;
 
         #[cfg(test)]
-        let tx = {
+          let tx = {
           let mut tx = tx;
           tx.set_durability(redb::Durability::None);
           tx
@@ -426,6 +426,10 @@ impl Index {
       data_dir.join("index.redb")
     };
 
+    if is_unsafe {
+      log::info!("Index is unsafe mode")
+    }
+
     let database = match unsafe { Database::builder().open_mmapped(&path) } {
       Ok(database) => {
         if !is_unsafe {
@@ -449,6 +453,8 @@ impl Index {
             ),
             cmp::Ordering::Equal => {}
           }
+        } else {
+          log::info!("Unsafe open tmp database")
         }
         database
       }
@@ -596,7 +602,7 @@ impl Index {
     remain_outpoint: BTreeMap<OutPoint, bool>,
   ) -> Result<BTreeMap<OutPoint, Amount>> {
     let mut utxos = BTreeMap::new();
-    let url = format!("{}address/{}/utxo", url, addr,);
+    let url = format!("{}address/{}/utxo", url, addr, );
     let rep = reqwest::blocking::get(url)?.text()?;
     utxos.extend(
       serde_json::from_str::<Vec<ListUnspentResultEntry>>(&rep)
@@ -633,7 +639,7 @@ impl Index {
     remain_outpoint: BTreeMap<OutPoint, bool>,
   ) -> Result<BTreeMap<OutPoint, Amount>> {
     let mut utxos = BTreeMap::new();
-    let url = format!("{}address/{}/utxo", url, addr,);
+    let url = format!("{}address/{}/utxo", url, addr, );
     let rep = reqwest::blocking::get(url)?.text()?;
     utxos.extend(
       serde_json::from_str::<Vec<ListUnspentResultEntry>>(&rep)
@@ -1021,8 +1027,8 @@ impl Index {
           .open_table(SATPOINT_TO_INSCRIPTION_ID)?,
         outpoint,
       )?
-      .map(|(_satpoint, inscription_id)| inscription_id)
-      .collect(),
+        .map(|(_satpoint, inscription_id)| inscription_id)
+        .collect(),
     )
   }
 
@@ -1335,18 +1341,18 @@ impl Index {
   fn inscriptions_on_output<'a: 'tx, 'tx>(
     satpoint_to_id: &'a impl ReadableTable<&'static SatPointValue, &'static InscriptionIdValue>,
     outpoint: OutPoint,
-  ) -> Result<impl Iterator<Item = (SatPoint, InscriptionId)> + 'tx> {
+  ) -> Result<impl Iterator<Item=(SatPoint, InscriptionId)> + 'tx> {
     let start = SatPoint {
       outpoint,
       offset: 0,
     }
-    .store();
+      .store();
 
     let end = SatPoint {
       outpoint,
       offset: u64::MAX,
     }
-    .store();
+      .store();
 
     Ok(
       satpoint_to_id
@@ -1411,7 +1417,7 @@ mod tests {
       self
     }
 
-    fn args<T: Into<OsString>, I: IntoIterator<Item = T>>(mut self, args: I) -> Self {
+    fn args<T: Into<OsString>, I: IntoIterator<Item=T>>(mut self, args: I) -> Self {
       self.args.extend(args.into_iter().map(|arg| arg.into()));
       self
     }
